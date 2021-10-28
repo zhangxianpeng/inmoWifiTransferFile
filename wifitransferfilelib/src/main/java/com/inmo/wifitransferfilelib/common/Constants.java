c04173c1ebed710917e6dc645f1bb7ee85f77a17
zhangxianpeng <302996244@qq.com>
2021-10-28 13:40:24 +0800

    端口1996

--- wifitransferfilelib/src/main/java/com/inmo/wifitransferfilelib/common/Constants.java
+++ wifitransferfilelib/src/main/java/com/inmo/wifitransferfilelib/common/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final String APPLICATION_ID = "com.inmo.wififiletransfer";
 
     //Socket连接的端口号
-    public static final int PORT = 1995;
+    public static final int PORT = 1996;
 }
